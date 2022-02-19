e6c5f677910bfa2c1934a26cafe74275c655c287
pilshikovdenis <denisnaga169@gmail.com>
2022-02-19 19:47:17 +0300

    Abstract classes completed

---
+++ src/com/chapter3/AbstractClasses.java
@@ -0,0 +1,114 @@
+package com.chapter3;
+
+import java.awt.*;
+
+public class AbstractClasses {
+    public static void main(String[] args) {
+        Monkey jojo = new Monkey("Jojo", true);
+        jojo.showInfo();
+
+        Horse gerda = new Horse("Gerda", 40);
+        gerda.showInfo();
+
+//        another example - figures
+        Rectangle rect = new Rectangle(57, 34, 100, 10);
+        System.out.println(rect.getPerimeter());
+        System.out.println(rect.getArea());
+        System.out.println();
+
+        Circle circle = new Circle(33, 12, 5);
+        System.out.println(circle.getPerimeter());
+        System.out.println(circle.getArea());
+
+
+    }
+}
+
+//abstract class
+abstract class Animal {
+    private String name;
+
+    public String getName() {
+        return name;
+    };
+    public void setName(String name) {
+        this.name = name;
+    }
+    public abstract void showInfo();
+
+    Animal(String name) {
+        this.name = name;
+    }
+}
+
+class Monkey extends Animal {
+    private boolean stayInZoo;
+
+    Monkey(String name, boolean isStayInZoo) {
+        super(name);
+        this.stayInZoo = isStayInZoo;
+    }
+    public void showInfo() {
+        System.out.println("I am monkey. I am stay in the zoo: " + stayInZoo + ". My name is  " + getName());
+    }
+
+}
+
+class Horse extends Animal {
+    private int maxSpeed;
+
+    Horse(String name, int maxSpeed) {
+        super(name);
+        this.maxSpeed = maxSpeed;
+    }
+    public void showInfo() {
+        System.out.println("I am horse. My name is " + getName() + ". My max speed is " + maxSpeed);
+    }
+}
+
+
+// another example
+abstract class Figure {
+    private float x;
+    private float y;
+
+    Figure(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public abstract float getPerimeter();
+    public abstract float getArea();
+}
+
+class Rectangle extends Figure {
+    private float width;
+    private float height;
+
+    Rectangle(float x, float y, float width, float height) {
+        super(x, y);
+        this.width = width;
+        this.height = height;
+    }
+    public float getPerimeter() {
+        return (width + height) * 2;
+    }
+    public float getArea() {
+        return width * height;
+    }
+}
+
+class Circle extends Figure {
+    private float radious;
+
+    Circle(float x, float y, float radious) {
+        super(x, y);
+        this.radious = radious;
+    }
+    public float getPerimeter() {
+        return 3.14f * radious * 2;
+    }
+    public float getArea() {
+        return 3.14f * radious * radious;
+    }
+}
--- src/com/chapter3/Inheritance.java
+++ src/com/chapter3/Inheritance.java
@@ -1,72 +1,72 @@
-package com.chapter3;
-
-public class Inheritance {
-    public static void main(String[] args) {
-        Human someMan = new Human("Викторов Генадий Петрович");
-        someMan.tellAboutHimself();
-
-        Programmer olga = new Programmer("Arapova Olga");
-        olga.tellAboutHimself();
-
-        Designer maksim = new Designer("Petrov Maksim Alekseevich");
-        maksim.tellAboutHimself();
-
-        Monkey kick = new Monkey();
-        kick.tellAboutHimself();
-
-//        динамическая диспетчеризация методов
-        Human georgy = new Designer("Morris Visconsa");
-        georgy.tellAboutHimself();
-    }
-}
-
-class Human {
-    private String initials;
-
-    public void setInitials(String initials) {
-        this.initials = initials;
-    }
-    public String getInitials() {
-
-        return initials;
-    }
-
-    Human(String initials) {
-        this.initials = initials;
-    }
-    public void tellAboutHimself() {
-        System.out.println("Я человек, могу работать");
-    }
-//    disable override this method in subclasses
-    public final void doSomething(){
-        System.out.println("Я чтото делаю");
-    }
-
-
-}
-
-class Programmer extends Human{
-
-    Programmer(String initials) {
-        super(initials);
-    }
-    public void tellAboutHimself() {
-        System.out.println("Я программист, пишу код");
-    }
-}
-
-class Designer extends Human {
-    Designer(String initials) {
-        super(initials);
-    }
-    public void tellAboutHimself() {
-        System.out.println("Я дизайнер, создаю пользовательские интерфейсы");
-    }
-}
-
-// inheritance disabled for subclasses
-final class Monkey {
-    void tellAboutHimself() {
-        System.out.println("I am monkey");
-    }
-}
+//package com.chapter3;
+//
+//public class Inheritance {
+//    public static void main(String[] args) {
+//        Human someMan = new Human("Викторов Генадий Петрович");
+//        someMan.tellAboutHimself();
+//
+//        Programmer olga = new Programmer("Arapova Olga");
+//        olga.tellAboutHimself();
+//
+//        Designer maksim = new Designer("Petrov Maksim Alekseevich");
+//        maksim.tellAboutHimself();
+//
+//        Monkey kick = new Monkey();
+//        kick.tellAboutHimself();
+//
+////        динамическая диспетчеризация методов
+//        Human georgy = new Designer("Morris Visconsa");
+//        georgy.tellAboutHimself();
+//    }
+//}
+//
+//class Human {
+//    private String initials;
+//
+//    public void setInitials(String initials) {
+//        this.initials = initials;
+//    }
+//    public String getInitials() {
+//
+//        return initials;
+//    }
+//
+//    Human(String initials) {
+//        this.initials = initials;
+//    }
+//    public void tellAboutHimself() {
+//        System.out.println("Я человек, могу работать");
+//    }
+////    disable override this method in subclasses
+//    public final void doSomething(){
+//        System.out.println("Я чтото делаю");
+//    }
+//
+//
+//}
+//
+//class Programmer extends Human{
+//
+//    Programmer(String initials) {
+//        super(initials);
+//    }
+//    public void tellAboutHimself() {
+//        System.out.println("Я программист, пишу код");
+//    }
+//}
+//
+//class Designer extends Human {
+//    Designer(String initials) {
+//        super(initials);
+//    }
+//    public void tellAboutHimself() {
+//        System.out.println("Я дизайнер, создаю пользовательские интерфейсы");
+//    }
+//}
+//
+//// inheritance disabled for subclasses
+//final class Monkey {
+//    void tellAboutHimself() {
+//        System.out.println("I am monkey");
+//    }
+//}
